980bf37004ec40efe3822fa94d7da45aa3d14910
kanedaaaa <samflynn19121@gmail.com>
2025-04-23 17:29:00 +0400

    feat: start writing the API

--- src/main.py
+++ src/main.py
@@ -1,10 +1,46 @@
-from mistral import prompt 
-from translator import translate_text
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Literal, List
 
-prompt_text = "Who split atom?"
+app = FastAPI()
 
-response = prompt(prompt_text)
+class Message(BaseModel):
+    role: Literal["user", "assistant"]
+    ge: str
+    en: str
 
-translated_response = translate_text(response, "ka")
+class ChatRequest(BaseModel):
+    new_message_ge: str
+    history: List[Message]
 
-print(translated_response)
+
+class ChatResponse(BaseModel):
+    history: List[Message]
+
+@app.post("/chat", response_model=ChatResponse)
+def chat(payload: ChatRequest):
+    try:
+        en_input = payload.new_message_ge # todo actually translate
+
+        updated_history = payload.history = [{
+            "role": "user",
+            "get": payload.new_message_ge,
+            "en": en_input
+        }]
+
+        mistral_messages = [
+            {"role": m.role, "content": m.en} for m in updated_history
+        ]
+
+        en_response = mistral_messages # todo actually call mistral 
+        ge_response = en_response # todo actually translate 
+
+        updated_history.append({
+            "role": "assistant",
+            "ge": ge_response,
+            "en": en_response
+        })
+
+        return {"history": updated_history}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
